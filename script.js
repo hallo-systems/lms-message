9db43561a6cd22d76e9f9fbe8537769641307cda
ymadd <Kb_yamato@protonmail.com>
2021-06-02 19:17:30 +0900

    removed jquery and add chapter

--- script.js
+++ script.js
@@ -5,12 +5,13 @@ const chapters = {
     "Chapter5":"イベント処理","Chapter6":"イベント処理","Chapter7":"イベント処理",
     "Chapter13":"変数","Chapter14":"変数","Chapter15":"変数",
     "Chapter16":"関数","Chapter17":"関数",
-    "アドバンスモード1":"応用プログラミング","アドバンスモード2":"アルゴリズムとデータ構造"
+    "アドバンスモード1":"応用プログラミング","アドバンスモード2":"アルゴリズムとデータ構造", "アドバンスモード3":"コンピュータ基礎"
 }
 
 var todayLessonType = "";
 var typingType = "";
 function displayHandler(lessonType) {
+    todayLessonType = lessonType;
     if (lessonType == "normal") {
         document.getElementById("normalDay").style.display = "inline";
         document.getElementById("presentationDay").style.display = "none";
@@ -33,67 +34,60 @@ function typingHandler(typing) {
 
 var message = "";
 function submitHandler() {
-    var chapter = document.getElementById("chapter").value;
-    var studentName = document.getElementById("studentname").value;
-    var pstudentName = document.getElementById("pstudentname").value;
+    let chapter = document.getElementById("chapter").value;
+    let studentName = document.getElementById("studentName").value;
+    let pstudentName = document.getElementById("pstudentname").value;
+    let comment = document.getElementById("comment").value;
+    let homeWork = document.getElementById("homeWork").value;
+    let phomeWork = document.getElementById("phomeWork").value;
+    let typing = document.getElementById("typingComentDescription").value;
+    let mission = document.getElementById("mission").text;
+    let pDescription = document.getElementById("pDescription").value;
+    let date = new Date();
+    let day = date.getDate();
+    let month = date.getMonth()+1;
+    let coachName = $('#coachname').val();
+    message = month+"/"+day+"<br>";
 
+    //normalday
+    if (todayLessonType == "normal"){
+        message += "【" + studentName + "さんレッスンレポート】<br>";
+        message += "<br>";
+        if(chapter=="Create"){
+            message += "本日はクリエイトモードを中心に行いました。<br>";
+            message += "<br>";
+            message += "本日のレッスン目標は、「"+mission+"」でした。<br>";
+        }else{
+            message += "本日は「"+ chapters[chapter] + "を学ぶ、" + chapter + "」に取り組みました。<br>";
+            message += "<br>";
+            message += "本日のレッスン目標は、「"+chapter+"を"+mission+"」でした。<br>";
+        }
+        message += comment + "<br>";
+        message += "<br>";
+        if (typingType == "yes"){
+            message += "タイピング練習では、"+typing+"<br>";
+        };
+        message += "自宅学習での目標は「"+homeWork+"」です。<br>";
+
+    //presentation
+    }else{
+        message += "【" + pstudentName + "さんレッスンレポート】<br>";
+        message += "<br>";
+        var pTitle = $('#title').val();
+        message += "本日は、発表会を行いました<br>"
+        message += pstudentName + "さんは、" + "[タイトル：" + pTitle +"]を発表してくれました。<br>"
+        message += "<br>"
+        message += pDescription + "<br>"
+        message += "<br>"
+        message += "自宅学習での目標は「"+phomeWork+"」です。<br>";
+    }
+    message += "<br>";
+    message += "よろしくお願いいたします。<br>";
+    message += "<br>";
+    message += "記入者：コーチ"+coachName;
+
+    document.getElementById('result').innerHTML = message;
 }
-// $('.submit').on('click', function(e){
-//     e.preventDefault();
-//     var comment = $('#comment').val();
-//     var homeWork = $('#homeWork').val();
-//     var phomeWork = $('#phomeWork').val();
-//     var typing = $('#typing-comment-description').val();
-//     var mission = $('#mission option:checked').text();
-//     var motivation = $('#motivation-description').val();
-//     var pDescription = $('#pDescription').val();
-//     var date = new Date();
-//     var month = date.getMonth()+1;
-//     var day = date.getDate();
-//     var coachName = $('#coachname').val();
-
-//     message = month+"/"+day+"<br>";
-//     if (todayLessonType == "normal"){
-//         message += "【" + studentName + "さんレッスンレポート】<br>";
-//         message += "<br>";
-//         if(chapter=="Create"){
-//             message += "本日はクリエイトモードを中心に行いました。<br>";
-//             message += "<br>";
-//             message += "本日のレッスン目標は、「"+mission+"」でした。<br>";
-//         }else{
-//             message += "本日は「"+ chapters[chapter] + "を学ぶ、" + chapter + "」に取り組みました。<br>";
-//             message += "<br>";
-//             message += "本日のレッスン目標は、「"+chapter+"を"+mission+"」でした。<br>";
-//         }
-//         message += comment + "<br>";
-//         if (motivation == "yes") {
-//             message += motivation + "<br>";
-//         };
-//         message += "<br>";
-//         if (TypingYesNo == "yes"){
-//             message += "タイピング練習では、"+typing+"<br>";
-//         };
-//         message += "自宅学習での目標は「"+homeWork+"」です。<br>";
-
-//     //presentation
-//     }else{
-//         message += "【" + pstudentName + "さんレッスンレポート】<br>";
-//         message += "<br>";
-//         var pTitle = $('#title').val();
-//         message += "本日は、発表会を行いました<br>"
-//         message += pstudentName + "さんは、" + "[タイトル：" + pTitle +"]を発表してくれました。<br>"
-//         message += "<br>"
-//         message += pDescription + "<br>"
-//         message += "<br>"
-//         message += "自宅学習での目標は「"+phomeWork+"」です。<br>";
-//     }
-//     message += "<br>";
-//     message += "よろしくお願いいたします。<br>";
-//     message += "<br>";
-//     message += "記入者：コーチ"+coachName;
-
-//     document.getElementById('result').innerHTML = message;
-// })
 
 function onClickCopy() {
     let pTag = document.getElementById('result');
